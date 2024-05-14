9841e14638501965345e407e3bb049a30990339b
Eric Freitas Avelino <128161569+Crire@users.noreply.github.com>
2024-05-13 23:35:00 -0300

    Delete assests directory

--- assests/js/main.js
+++
@@ -1,7 +0,0 @@
-function redireconaCadastro(){
-    document.querySelector('#btn-inicia').addEventListener('click', function(){
-        window.location.href = "./cadastro.html";
-    })
-    
-}
-redireconaCadastro();
